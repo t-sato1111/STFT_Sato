1754022cff478000cd0f538b044da3d74756b64a
t-sato1111 <takato.s1111@icloud.com>
2026-04-17 13:09:34 +0900

    パワースペクトログラムの計算

---
+++ STFT.m
@@ -0,0 +1,28 @@
+clear; close all; clc;
+
+% wavファイル読み込み
+[audioSig, fs] = audioread("sig1.wav"); %このコマンドでsigに入れると基本縦行列
+signalLength = length(audioSig); % 信号長確保
+
+% 各種パラメータを定義
+windowLength = 2 ^ 12; % 窓の幅(2の乗数だと動作が早い)
+shiftLength = windowLength / 2; % シフトする幅
+
+% ゼロパディング
+prepaddedSig = padarray(audioSig, shiftLength, 0, "pre"); % 前側に0を必要なだけ追加
+paddedSig = padarray(prepaddedSig, windowLength - 1, 0, "post"); % 後ろ側に0を必要なだけ追加
+
+% 信号分割処理
+numFlames = ceil((windowLength / 2 + signalLength) / shiftLength); % シフトする回数(ceilで切り上げ)
+win = hann(windowLength); % ハン窓作成
+S = zeros(windowLength, numFlames); 
+for i = 1 : numFlames % 1~numFlamsまで繰り返し
+    startIndex = 1 + (i-1) * shiftLength; % 切り取る範囲のはじめ
+    endIndex = startIndex + windowLength - 1; % 切り取る範囲の終わり
+    S(:, i) = paddedSig(startIndex : endIndex); % i列目のすべての行に代入 
+end
+
+% 窓かけ処理
+winS = win .* S; % 窓かけ
+absWinS = abs(winS); % 各要素の絶対値
+pwS = absWinS .^ 2; % パワースペクトログラムの計算
